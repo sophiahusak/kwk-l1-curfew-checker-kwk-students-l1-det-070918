08dedc9336dd1022b66de502b7c99ba521a52e7a
sophiahusak <72futbolplayer@gmail.com>
2018-07-11 17:22:46 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,37 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "time is less than 11"
+  elsif time == 11
+    puts "time is 11"
+  else
+    puts "time is not less than or equal to 11"
+  end
 end
+simple_curfew_checker(11)
+
 
 def curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "time is less than 11"
+  else
+    puts "time is not less than 11"
+  end
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "time is less than 11"
+  elsif time == 11
+    puts "time is 11"
+  else
+    puts "time is not less than or equal to 11"
+  end
 end
 
 def deluxe_curfew_checker(time)
-  # code goes here
+   
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  
 end
